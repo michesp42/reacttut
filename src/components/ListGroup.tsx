e36740a8b47ea0290d4a4e3fe6f8b2458ca4dd35
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-18 16:52:12 +0800

    create ListGroup component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+function ListGroup() {
+  return (
+    <ul className="w-96">
+      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+        An item
+      </li>
+      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+        A second item
+      </li>
+      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+        A third item
+      </li>
+      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+        A fourth item
+      </li>
+      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+        And a fifth one
+      </li>
+    </ul>
+  );
+}
+
+export default ListGroup;
