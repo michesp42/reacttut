991f072109c46ecce0b1f4a6d52c3e12ef0b7b06
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-18 17:02:32 +0800

    format

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,24 +1,24 @@
 function ListGroup() {
   return (
     <>
-    <h1 className="text-xl">List</h1>
-    <ul className="w-96">
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-        An item
-      </li>
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-        A second item
-      </li>
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-        A third item
-      </li>
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-        A fourth item
-      </li>
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-        And a fifth one
-      </li>
-    </ul>
+      <h1 className="text-xl">List</h1>
+      <ul className="w-96">
+        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+          An item
+        </li>
+        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+          A second item
+        </li>
+        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+          A third item
+        </li>
+        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+          A fourth item
+        </li>
+        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
+          And a fifth one
+        </li>
+      </ul>
     </>
   );
 }
