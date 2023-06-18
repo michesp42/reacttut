529ff67d91a51177bbc57f4304fa6881d32931b6
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-18 17:14:08 +0800

    render lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,18 @@
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <>
-      <h1 className="text-xl">List</h1>
+      <h1 className="text-red-400 text-3xl">List</h1>
       <ul className="w-96">
-        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-          An item
-        </li>
-        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-          A second item
-        </li>
-        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-          A third item
-        </li>
-        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-          A fourth item
-        </li>
-        <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4">
-          And a fifth one
-        </li>
+        {items.map((item) => (
+          <li
+            key={item}
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 text-lg"
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
